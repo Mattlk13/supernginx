ec2668a3e1bc7feb3433a21f2dcbad35abb3b894
WCMinor <risingofthemoon@gmail.com>
2015-06-07 00:10:18 +0100

    README changed

--- README.md
+++ README.md
@@ -15,3 +15,7 @@ This has been tested under:
 
 This takes a while to start.
 
+You need to redirect your local 80 port to the port 8080 and add panel.supernginx.com to your /etc/hosts file pointing to localhost ex:
+
+#echo "127.0.0.1	panel.supernginx.com" >> /etc/hosts
+#ssh user@localhost -L 80::8080
--- puppet/localmodules/roles/manifests/supernginx.pp
+++ puppet/localmodules/roles/manifests/supernginx.pp
@@ -12,43 +12,5 @@ nginx::resource::upstream { 'panel':
 nginx::resource::vhost { 'panel.supernginx.com':
   proxy => 'http://panel',
 }
-#
-#  class { '::tomcat':
-#    install_from_source => false,
-#  } ->
-#  tomcat::instance { 'default':
-#    package_name => 'tomcat',
-#    require      => Class['epel'],
-#  } ->
-#  tomcat::service { 'default':
-#    use_jsvc     => false,
-#    use_init     => true,
-#    service_name => 'tomcat',
-#  }
-#
-#  tomcat::setenv::entry { 'HELLO_DATEURL':
-#    value       => 'http://sleepy-thicket-8107.herokuapp.com',
-#    require     => Class['tomcat'],
-#    config_file => '/etc/sysconfig/tomcat',
-#    notify      => Tomcat::Service['default'],
-#  }
-#  tomcat::setenv::entry { 'HELLO_CONN_TIMEOUT':
-#    value       => '1000',
-#    require     => Class['tomcat'],
-#    config_file => '/etc/sysconfig/tomcat',
-#    notify      => Tomcat::Service['default'],
-#  }
-#  tomcat::setenv::entry { 'HELLO_READ_TIMEOUT':
-#    value       => '5000',
-#    require     => Class['tomcat'],
-#    config_file => '/etc/sysconfig/tomcat',
-#    notify      => Tomcat::Service['default'],
-#  }
-#
-#  tomcat::war { 'hello-world.war':
-#    war_source    => 'https://s3-eu-west-1.amazonaws.com/bashton-engineering-test/bashton-hello-world_2.10-0.1.1-SNAPSHOT.war',
-#    catalina_base => '/var/lib/tomcat',
-#    require       => Class['tomcat'],
-#  }
 
 }
